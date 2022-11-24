19455d6c74612a5cb089bea8701f738da97659e9
Marta Perelló <100278106+Martaperello@users.noreply.github.com>
2022-11-24 13:35:24 +0100

    first commit

---
+++ 1.js
@@ -0,0 +1,10 @@
+function miFunc() {
+    alert('¡Enhorabuena!');
+  }
+
+var colors = require('colors');
+ 
+console.log('The Beatles'.green);
+
+const Color = require('color');
+const color = Color('rgb(255, 255, 255)')
